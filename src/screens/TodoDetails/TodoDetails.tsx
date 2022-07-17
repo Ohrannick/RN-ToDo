cef0eb88e566ec45bdb6942d98226a7641ae8881
ohrannick <ohrannick@gmail.com>
2022-07-17 18:43:36 +0300

    add lesson 5

--- src/screens/TodoDetails/TodoDetails.tsx
+++ src/screens/TodoDetails/TodoDetails.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Button, ScrollView, Text} from 'react-native';
+import {Button, ScrollView, Switch, Text, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {launchImageLibrary} from 'react-native-image-picker';
 
@@ -9,12 +9,62 @@ import {changeTodo} from '../../store/actions';
 import {selectTodoById} from '../../store/selectors';
 import {TodoDetailsProps} from './TodoDetails.types';
 import {Gallery} from '../../components/Gallery/Gallery';
+import notifee, { AndroidImportance, RepeatFrequency, TimestampTrigger, TriggerType } from '@notifee/react-native';
 
 export const TodoDetails = ({route, navigation}: TodoDetailsProps) => {
   const dispatch = useDispatch();
 
   const todo = useSelector(selectTodoById(route.params.todoId));
   const [editedTitle, setEditedTitle] = useState(todo.title);
+
+  const handleSetPush = async () => {
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Default Channel',
+    importance: AndroidImportance.HIGH,
+  });
+
+  const date = new Date();
+  date.setHours(12);
+  date.setMinutes(0);
+  date.setSeconds(0);
+
+  const trigger: TimestampTrigger = {
+    type: TriggerType.TIMESTAMP,
+    timestamp: date.getTime(),
+    repeatFrequency: RepeatFrequency.DAILY, // repeat once a week
+  };
+  await notifee.createTriggerNotification(
+    {
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        importance: AndroidImportance.HIGH,
+        asForegroundService: true,
+        pressAction: {
+          id: 'default',
+        },
+      },
+      data: {
+        id: todo.id+'',
+      },
+    },
+    trigger,
+  );
+};
+
+  const handleCancelPush = async () => {
+    await notifee.cancelTriggerNotification(todo.id+'');
+  };
+  const handleSwitch = async () => {
+    if (todo.notificationIsOn) {
+      await handleCancelPush();
+    } else {
+      await handleSetPush();
+    }
+    dispatch(changeTodo({...todo, notificationIsOn: !todo.notificationIsOn}));
+  };
 
   useEffect(() => {
     navigation.setOptions({
@@ -67,6 +117,10 @@ export const TodoDetails = ({route, navigation}: TodoDetailsProps) => {
 
   return (
     <ScrollView>
+      <View>
+        <Text>Notification</Text>
+        <Switch value={todo.notificationIsOn} onChange={handleSwitch} />
+      </View>
       <TextField onChangeText={setEditedTitle} initialValue={todo.title} />
       <Text style={{fontSize: 20}}>Hello, {todo.title}</Text>
       <Gallery onPress={handleImagePress} imgs={todo.imgs} />
